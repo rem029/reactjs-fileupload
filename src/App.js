83b862797077d0f6c209b5e189c1d0d5a98ce834
rem029 <elawrenceponce@gmail.com>
2021-10-26 15:43:35 +0300

    main

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
+  const [image, setImage] = useState(null);
+  const [imagePreview, setImagePreview] = useState(null);
+  const fileInputRef = useRef();
+
+  const onButtonClick = (event) => {
+    event.preventDefault();
+    fileInputRef.current.click();
+  };
+
+  const onFileChange = (event) => {
+    const file = event.target.files[0];
+
+    if (file && file.type.substring(0, 5) === 'image') {
+      setImage(file);
+    } else {
+      setImage(null);
+    }
+  };
+
+  useEffect(() => {
+    console.log('image update', image);
+
+    if (image) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImagePreview(reader.result);
+      };
+      reader.readAsDataURL(image);
+    } else {
+      setImagePreview(null);
+    }
+  }, [image]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <button onClick={onButtonClick}>Add Images</button>
+      {imagePreview && <img src={imagePreview} width="50%" height="auto" />}
+      <input
+        ref={fileInputRef}
+        type="file"
+        style={{ display: 'none' }}
+        accept={'image/*'}
+        onChange={onFileChange}
+      ></input>
+    </form>
   );
 }
 
